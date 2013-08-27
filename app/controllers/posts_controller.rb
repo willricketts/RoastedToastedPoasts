5bfc185a3fbe19121ef250118db65289cf4804a3
Will Ricketts <william.w.ricketts@gmail.com>
2013-08-27 09:32:14 -0400

    yao

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(params[:post])
+        
+        if @post.save
+            redirect_to posts_path, :notice => "Nice one. Way to roast that poast!"
+        else
+            render "new"
+        end
+    end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+        if @post.update_attributes(params[:post])
+            redirect_to posts_path, :notice => "Nice one. Way to roast that poast!"
+        else
+            render "edit"
+        end
+    end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path, :notice => "That poast is toast!"
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Willblog::Application.config.secret_token = '758861faef318cb9ec2e1bfdd6f4c3dc4faff6aaa12677ec349b7cea2ac73f265795983ebcaeea307c0e394716ad9ff5d904c803e3306017a01811ec5d561fad'
